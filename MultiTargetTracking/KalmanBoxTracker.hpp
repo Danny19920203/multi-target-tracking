607a321254e34e3a508aba3197d6ea01181763b0
yangzhixiong <yangzhixiong@sensenets.com>
2016-03-16 18:53:58 +0800

    change the KalmanBoxTracker file

---
+++ MultiTargetTracking/KalmanBoxTracker.hpp
@@ -0,0 +1,32 @@
+/**This file implements the kalman tracker with opencv
+**/
+#ifndef KALMANBOXTRACKER_HPP
+#define KALMANBOXTRACKER_HPP
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/opencv.hpp>
+
+
+
+
+class KalmanBoxTracker
+{
+    public:
+        KalmanBoxTracker(cv::Rect )
+
+
+        KalmanFiler* KF;
+
+
+
+};
+
+
+
+
+
+
+#endif //KALMANBOXTRACKER_HPP
+
